26fbdf05d6af7ec75e74fefb286acabd6697c87e
JDigar <jdigar96@gmail.com>
2022-05-20 18:55:33 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  document.querySelector(".top-suit").innerHTML = randomSimbol;
+  document.querySelector(".bottom-suit").innerHTML = randomSimbol;
 };
+
+const generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+const generateRandomsimbol = () => {
+  let simbol = ["♦", "♥", "♠", "♣"];
+  let indexSimbol = Math.floor(Math.random() * simbol.length);
+  return simbol[indexSimbol];
+};
+
+let randomSimbol = generateRandomsimbol();
